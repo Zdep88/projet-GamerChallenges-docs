ca72f67d349191af2f655950f6c2069643e4fcb0
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-02-25 10:16:32 +0100

    ajout du MPD

---
+++ MPD.sql
@@ -0,0 +1,52 @@
+BEGIN;
+DROP TABLE IF EXISTS game CASCADE;
+DROP TABLE IF EXISTS player CASCADE;
+DROP TABLE IF EXISTS challenge CASCADE;
+DROP TABLE IF EXISTS proposition CASCADE;
+DROP TABLE IF EXISTS vote_challenge CASCADE;
+DROP TABLE IF EXISTS vote_proposition CASCADE;
+COMMIT;
+
+BEGIN;
+CREATE TABLE game (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    date INTEGER NOT NULL,
+    platform TEXT NOT NULL,
+    picture TEXT NOT NULL
+);
+
+CREATE TABLE player (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE challenge (
+    id INTEGER PRIMARY KEY,
+    player_id INTEGER REFERENCES player(id) ON DELETE CASCADE,
+    game_id INTEGER REFERENCES game(id) ON DELETE CASCADE,
+    name TEXT NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE proposition (
+    id INTEGER PRIMARY KEY,
+    challenge_id INTEGER REFERENCES challenge(id) ON DELETE CASCADE,
+    player_id INTEGER REFERENCES player(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    description TEXT,
+    url_video TEXT NOT NULL
+    );
+
+CREATE TABLE vote_challenge (
+    challenge_id INTEGER REFERENCES challenge(id) ON DELETE CASCADE,
+    player_id INTEGER REFERENCES player(id) ON DELETE CASCADE
+);
+
+CREATE TABLE vote_proposition (
+    player_id INTEGER REFERENCES player(id) ON DELETE CASCADE,
+    proposition_id INTEGER REFERENCES proposition(id) ON DELETE CASCADE
+);
+COMMIT;
